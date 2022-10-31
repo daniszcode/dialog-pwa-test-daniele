da5563994ae14d99503fa1b8ca1692b6b61b4161
daniszcode <daniele.marques@creditas.com>
2022-10-31 16:33:35 +0000

    fix: code corrections

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -14,13 +14,8 @@ root.render(
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<CardHome />} />
-          {/* <Route path="/profile" element={<ProfileScreen />} /> */}
         </Routes>
       </BrowserRouter>
     </ApolloProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
